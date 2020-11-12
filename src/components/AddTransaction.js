c3abc310ceeae777558abaeebdc9a3ce80ca016e
Darkweb96 <66476631+Darkweb96@users.noreply.github.com>
2020-11-12 20:44:30 +0300

    made better

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -7,7 +7,7 @@ import { GlobalContext } from '../context/GlobalState';
 export const AddTransaction = () => {
 
     const [description, setDescription] = useState('');
-    const [transactionAmount, setTransactionAmount] = useState('');
+    const [transactionAmount, setTransactionAmount] = useState(0);
 
     const { addTransaction } = useContext(GlobalContext);
 
